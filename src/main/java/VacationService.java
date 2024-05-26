b2630ec48eb39641eadd7018a68334c2ee1e35a5
Test <bloodreighn965@gmail.com>
2024-05-26 19:57:21 +0300

    Update

--- src/main/java/VacationService.java
+++ src/main/java/VacationService.java
@@ -1,22 +1,21 @@
-    package ru.netology.services ;
 
-    public class VacationService {
+public class VacationService {
 
-        public int calculate(int income, int expenses, int threshold) {
-            int months = 0;
-            int cash = 0;
-            for (int i = 0; i < 12; i++) {
-                if (cash >= threshold) {
-                    months++;
-                    cash -= expenses;
-                    cash/= 3; // cash =cash / 3
-                } else {
-                    cash += income;
-                    cash -= expenses;
+    public int calculate(int income, int expenses, int threshold) {
+        int months = 0;
+        int cash = 0;
+        for (int i = 0; i < 12; i++) {
+            if (cash >= threshold) {
+                months++;
+                cash -= expenses;
+                cash /= 3; // cash =cash / 3
+            } else {
+                cash += income;
+                cash -= expenses;
 
-                }
             }
-            return months;
         }
+        return months;
     }
+}
 
--- src/test/java/VacationServiceTest.java
+++ src/test/java/VacationServiceTest.java
@@ -1,7 +1,16 @@
-package ru.netology.services ;
+package ru.netology.services;
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class VacationServiceTest {
     @Test
+    public void test() {
+        VacationService service = new VacationService();
 
+        int expected = 3;
+        Object income = null;
+        int actual = service.calculate(income:10_000, expences:3_000, threshold:20_000);
+        Assertions.assertEquals(expected, actual);
+    }
 }
